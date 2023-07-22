4bc2ec863632dda567706fd20339ae6f3dfa4bf6
Artem Ovchinnikov <artem.o@amayasoft.uz>
2023-07-22 20:51:03 +0300

    Bootstrap and entry points added

---
+++ Assets/Runtime/GameLifetime/BootstrapLifetimeScope.cs
@@ -0,0 +1,23 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace DefendTheWave.GameLifetime
+{
+	public class BootstrapLifetimeScope : LifetimeScope
+	{
+		private LifetimeScope _levelLifetimeScope;
+		
+		protected override void Configure(IContainerBuilder builder)
+		{
+			_levelLifetimeScope = CreateLevelLifetimeScope();
+				
+			LifetimeScope CreateLevelLifetimeScope()
+			{
+				return CreateChild(levelScopeBuilder =>
+				{
+					
+				});
+			}
+		}
+	}
+}
---
+++ Assets/Runtime/GameLifetime/GameEntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+using VContainer.Unity;
+
+namespace DefendTheWave.GameLifetime
+{
+	public class GameEntryPoint : MonoBehaviour
+	{
+		[SerializeField] private LifetimeScope _initialLifetimeScope;
+
+		private void Awake()
+		{
+			_initialLifetimeScope.Build();
+		}
+	}
+}
