56e9de80cf38d3237d4c624c00e3b3454e56182f
Artem Ovchinnikov <artem.o@amayasoft.uz>
2023-07-26 18:17:32 +0300

    Added non-clamped player movement

--- Assets/Runtime/Data/Settings/SpawnablePlayerSettings.cs
+++ Assets/Runtime/Data/Settings/SpawnablePlayerSettings.cs
@@ -7,10 +7,10 @@ namespace DefendTheWave.Data.Settings
 	[CreateAssetMenu(fileName = nameof(SpawnablePlayerSettings), menuName = GameConstants.ScriptableObjectsRoot + nameof(SpawnablePlayerSettings), order = 0)]
 	public class SpawnablePlayerSettings : BaseSpawnableEntitySettings
 	{
-		[field: SerializeField, Range(1f, 3f)] public float BlastRadius { get; private set; }
-		[field: SerializeField, Range(0.25f, 5f)] public float RateOfFire { get; private set; }
-		[field: SerializeField, Range(1, 3f)] public int Damage { get; private set; }
-		[field: SerializeField, Range(1f, 5f)] public float ProjectileSpeed { get; private set; }
-		[field: SerializeField, Range(1f, 5f)] public float PlayerSpeed { get; private set; }
+		[field: SerializeField, Range(1, 3f)] public int Damage { get; private set; }	
+		[field: SerializeField, Min(0f)] public float BlastRadius { get; private set; }
+		[field: SerializeField, Min(0f)] public float RateOfFire { get; private set; }
+		[field: SerializeField, Min(0f)] public float ProjectileSpeed { get; private set; }
+		[field: SerializeField, Min(0f)] public float PlayerSpeed { get; private set; }
 	}
 }
--- Assets/Runtime/Player/Movement/PlayerMovementController.cs
+++ Assets/Runtime/Player/Movement/PlayerMovementController.cs
@@ -1,5 +1,6 @@
 ﻿using Cysharp.Threading.Tasks.Linq;
 
+using DefendTheWave.Data.Settings;
 using DefendTheWave.Input;
 
 using UnityEngine;
@@ -14,21 +15,33 @@ namespace DefendTheWave.Player.Movement
 	public class PlayerMovementController : Disposable, IInitializable
 	{
 		[Inject] private readonly IInputService _inputService;
+		[Inject] private readonly PlayerContainer _playerContainer;
+		[Inject] private readonly SpawnablePlayerSettings _playerSettings;
 
 		private Transform _playerTransform;
+		
+		private float _playerSpeed;
 		
 		void IInitializable.Initialize()
 		{
 			CompositeDisposable.Add(_inputService.MovementVector.WithoutCurrent().Subscribe(MovePlayer));
+			CompositeDisposable.Add(_playerContainer.Player.Subscribe(SetPlayerTransform));
+
+			_playerSpeed = _playerSettings.PlayerSpeed;
 		}
 
 		private void SetPlayerTransform(PlayerView playerView)
 		{
+			_playerTransform = playerView.transform;
 		}
 
 		private void MovePlayer(Vector2 movementVector)
 		{
-			_playerTransform.Translate(movementVector * 10 * Time.deltaTime);
+			#if UNITY_EDITOR
+			_playerTransform.Translate(movementVector * _playerSettings.PlayerSpeed * Time.deltaTime);
+			#else
+			_playerTransform.Translate(movementVector * _playerSpeed * Time.deltaTime);
+			#endif
 		}
 	}
 }
